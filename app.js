97d36d8663f049001f371d61028b69a9c88edfbf
Jacek Walasik <jacekwalasik89@gmail.com>
2019-09-17 21:27:21 +0200

    implement mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("5d7cfe9e3be97c2cf41ac654")
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+  User.findById("5d8121fdc5ed9514dc7ddcce")
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,6 +32,21 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect('mongodb+srv://elPaleniozord:xqwzts@cluster0-mtlt1.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result => {
+    User.findOne().then(user => {
+      if(!user) {
+        const user = new User({
+          name: 'elPaleniozord',
+          email: 'jacekwalasik89@gmail.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save()
+      }
+    })    
+    app.listen(3000)
+  })
+  .catch(err=>console.log(err))
